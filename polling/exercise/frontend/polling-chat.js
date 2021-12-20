9e5e43a9fb94748319716f2b190fb2f5063d28b7
Andreea Eftene <hi@pixelle.ro>
2021-12-20 13:58:08 +0100

    implement simple polling

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -20,8 +20,16 @@ async function postNewMsg(user, text) {
 }
 
 async function getNewMsgs() {
-  // poll the server
-  // write code here
+  let json;
+  try {
+    const res = await fetch("/poll");
+    json = await res.json();
+  } catch (e) {
+    console.error("polling error", e);
+  }
+  allChat = json.msg;
+  render();
+  setTimeout(getNewMsgs, INTERVAL);
 }
 
 function render() {
